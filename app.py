e5d83178d2f3ad903676466734c57200259e0bbd
rohan2734 <jagandevaki1@gmail.com>
2020-12-22 15:20:27 +0530

    added templates

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>Hello World</h1>"
 
+@app.route('/<string:name>')
+def greet(name):
+    return 'Hello {name} '
+
+
 if __name__ == '__main__':
     app.run(debug=True)
