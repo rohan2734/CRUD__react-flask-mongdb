8e84d8e97d09f285912bc4f0d2b0aac4ddfb1f50
rohan2734 <jagandevaki1@gmail.com>
2020-12-23 16:37:26 +0530

    added database

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask,render_template,request,redirect,url_for
 from forms import Todo
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'password'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/test.db'
+
+db = SQLAlchemy(app)
+
+class TodoModel(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(240)) 
+
+    def __str__(self):
+        return f'{self.content,{self.id}}'
+
 
 @app.route('/',methods=['GET','POST'] )
 def hello_world():
