43afa4fad903adcc1fb244b060ca65ba3bfcc197
rohan2734 <jagandevaki1@gmail.com>
2020-12-25 13:54:24 +0530

    added passandusers info

--- flask_api_mongodb/api_mongo.py
+++ flask_api_mongodb/api_mongo.py
@@ -1,14 +1,25 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
+from api_constants import mongodb_user,mongodb_password,mongodb_dbname
 
 app=Flask(__name__)
 
 database_name = "flaskAndMongo"
-DB_URI = ""
-app.config["M"]
+DB_URI = f"mongodb+srv://{mongodb_user}:{mongodb_password}@cluster0.3kcv6.mongodb.net/{mongodb_dbname}?retryWrites=true&w=majority"
+
+app.config["MONGODB_HOST"]=DB_URI
 
 db = MongoEngine()
 db.init_app(app)
+
+"""
+ Sample request body
+ {
+     "book_id" : 1,
+     "name":"A game of thrones",
+     "author":"George R Martin"
+ }
+"""
 
 if __name__ == '__main__':
     app.run(debug=True)
